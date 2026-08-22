af46ad857bff3fcc7b343416b4e4108b3a02f9a0
munta6178-hub <munta6178@gmail.com>
2026-08-22 08:35:20 -0700

    Add files via upload

--- src/__root.tsx
+++ src/__root.tsx
@@ -9,8 +9,8 @@ import {
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
 
-import appCss from "@/styles.css?url";
-import { reportLovableError } from "@/lib/lovable-error-reporting";
+import appCss from "../styles.css?url";
+import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Nav } from "@/components/Nav";
 import { Footer } from "@/components/Footer";
 
